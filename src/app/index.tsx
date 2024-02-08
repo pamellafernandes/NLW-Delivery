1f07bb8b06752f8e232e023380d44806dfa06230
pamellafernandes <pamellafernandes118@gmail.com>
2024-02-07 22:14:16 -0300

    feat: Added the screen of the cart

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,14 +3,14 @@ import { useState, useRef } from "react";
 import { View, FlatList, SectionList, Text } from "react-native";
 import { CategoryButton } from "@/components/category-button";
 import { useCartStore } from "@/stores/cart-store";
-import { CATEGORIES, MENU } from "@/utils/data/products";
+import { CATEGORIES, MENU, ProductProps } from "@/utils/data/products";
 import { Product } from "@/components/product";
 import { Link } from "expo-router";
 
 export default function Home() {
   const cartStore = useCartStore();
   const [category, setCategory] = useState(CATEGORIES[0]);
-  const sectionListRef = useRef<SectionList>(null);
+  const sectionListRef = useRef<SectionList<ProductProps>>(null);
   const cartQuantityItems = cartStore.products.reduce(
     (total, product) => total + product.quantity,
     0
